966b4389dd0248aa9943cb8a269e6d6d7117533d
MalazMutwallyMohammedArbab <malazarbab870@gmail.com>
2022-12-17 12:28:57 +0300

    edit images

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -4,28 +4,28 @@ import Image from "../Components/Images";
 export default function Registration() {
   return (
     <div className='container font-regu'>
-      <div className='text-3xl text-[red] font-semibold'>قسم Registration & ESR:</div>
+      <div className='text-3xl text-red font-semibold'>قسم Registration & ESR:</div>
       <Image className='w-80 m-auto mt-6' src='images/registration-place.jpg' alt='' />
       <div className='text-center'>Registration place</div>
 
-      <div className='text-2xl text-[red] mt-7 mb-3 font-semibold'>1- Registration:</div>
-      <div className='text-xl mb-5'><span className='text-[red] font-semibold'>تجهيز الدفتر: </span>كتابة التاريخ واليوم وعمل جدول بالبيانات (رقم الباتش، اسم المريض، رقم الكمبيوتر، الفحوصات المطلوبة)</div>
+      <div className='text-2xl text-red mt-7 mb-3 font-semibold'>1- Registration:</div>
+      <div className='text-xl mb-5'><span className='text-red font-semibold'>تجهيز الدفتر: </span>كتابة التاريخ واليوم وعمل جدول بالبيانات (رقم الباتش، اسم المريض، رقم الكمبيوتر، الفحوصات المطلوبة)</div>
       <Image className='w-80 m-auto' src='images/registration.jpg' alt='img' />
       <div className='text-center'>Registration</div>
 
-      <div className='text-xl my-5'>بعد إحضار العينات من الباتش إلى داخل المعمل <span className='text-[red]'>يتم التأكد من تطابق البيانات في الاستمارة مع ال container لكل مريض، <br /> إذا لم تتطابق البيانات يتم الرجوع للباتش والتعديل </span>وإذا تطابقت يتم تسجيل بيانات المريض (رقم الباتش واسم المريض ورقم الكمبيوتر والفحص المطلوب) في الدفتر</div>
+      <div className='text-xl my-5'>بعد إحضار العينات من الباتش إلى داخل المعمل <span className='text-red'>يتم التأكد من تطابق البيانات في الاستمارة مع ال container لكل مريض، <br /> إذا لم تتطابق البيانات يتم الرجوع للباتش والتعديل </span>وإذا تطابقت يتم تسجيل بيانات المريض (رقم الباتش واسم المريض ورقم الكمبيوتر والفحص المطلوب) في الدفتر</div>
 
-      <div className='text-xl mb-3'><span className='text-[red] font-semibold'>أولا: في حالة كان الفحص المطلوب هو CBC: <br /></span>يتم إعطاء العينة للشخص المسؤول عن عمل فحص ال  CBC</div>
-      <div className='text-xl mb-3'><span className='text-[red]'>إذا كانت نتيجة فحص ال CBC طبيعية </span>يتم تدبيس النتيجة مع الاستمارة وإعطائها للمسؤول عن تسجيل النتيجة في الكمبيوتر</div>
-      <div className='text-xl'><span className='text-[red]'>إذا كانت نتيجة فحص ال CBC غير طبيعية </span>يتم كتابة الاختصار ab بجانب اسم المريض في دفتر التسجيل ويتم إعطاء النتيجة مع الاستمارة مع العينة للمسؤول عن ال Abnormal</div>
+      <div className='text-xl mb-3'><span className='text-red font-semibold'>أولا: في حالة كان الفحص المطلوب هو CBC: <br /></span>يتم إعطاء العينة للشخص المسؤول عن عمل فحص ال  CBC</div>
+      <div className='text-xl mb-3'><span className='text-red'>إذا كانت نتيجة فحص ال CBC طبيعية </span>يتم تدبيس النتيجة مع الاستمارة وإعطائها للمسؤول عن تسجيل النتيجة في الكمبيوتر</div>
+      <div className='text-xl'><span className='text-red'>إذا كانت نتيجة فحص ال CBC غير طبيعية </span>يتم كتابة الاختصار ab بجانب اسم المريض في دفتر التسجيل ويتم إعطاء النتيجة مع الاستمارة مع العينة للمسؤول عن ال Abnormal</div>
 
-      <div className='text-xl my-7'><span className='text-[red] font-semibold'>ثانيا: في حالة كان الفحص المطلوب هو BF: <br /></span>يتم إعطاء استمارة المريض مع العينة للشخص المسؤول عن ال Abnormal</div>
+      <div className='text-xl my-7'><span className='text-red font-semibold'>ثانيا: في حالة كان الفحص المطلوب هو BF: <br /></span>يتم إعطاء استمارة المريض مع العينة للشخص المسؤول عن ال Abnormal</div>
 
-      <div className='text-xl mb-7'><span className='text-[red] font-semibold'>ثالثا: في حالة كان الفحص المطلوب هو Sickiling test أو Reticulocyte count: <br /></span>يتم إعطاء استمارة المريض مع العينة للشخص المسؤول عن ال Special</div>
+      <div className='text-xl mb-7'><span className='text-red font-semibold'>ثالثا: في حالة كان الفحص المطلوب هو Sickiling test أو Reticulocyte count: <br /></span>يتم إعطاء استمارة المريض مع العينة للشخص المسؤول عن ال Special</div>
 
-      <div className='text-xl mb-7'><span className='text-[red] font-semibold'>رابعا: في حالة كان الفحص المطلوب هو ESR: <br /></span>يتم تسجيل البيانات مرة أخرى في دفتر مختلف وهو مخصص لفحص ESR والبيانات هي (رقم الباتش واسم المريض وزمن قراءة الفحص والنتيجة)</div>
+      <div className='text-xl mb-7'><span className='text-red font-semibold'>رابعا: في حالة كان الفحص المطلوب هو ESR: <br /></span>يتم تسجيل البيانات مرة أخرى في دفتر مختلف وهو مخصص لفحص ESR والبيانات هي (رقم الباتش واسم المريض وزمن قراءة الفحص والنتيجة)</div>
 
-      <div className='text-2xl text-[red] mb-3 font-semibold'>2- فحص ESR:</div>
+      <div className='text-2xl text-red mb-3 font-semibold'>2- فحص ESR:</div>
       <Image className='w-80 m-auto' src='images/esr-test.jpg' alt='img' />
       <div className='text-center'>ESR = 85 mmol / 1/2 hr</div>
 
--- src/Pages/Special.jsx
+++ src/Pages/Special.jsx
@@ -4,8 +4,8 @@ import Image from "../Components/Images";
 export default function Special() {
   return (
     <div className='container font-regu'>
-      <div className='text-[red] text-4xl font-semibold'>قسم Special</div>
-      <div className='mt-4 text-2xl font-semibold'><span className='text-[red]'>1- </span>Sickiling test</div>
+      <div className='text-red text-4xl font-semibold'>قسم Special</div>
+      <div className='mt-4 text-2xl font-semibold'><span className='text-red'>1- </span>Sickiling test</div>
       <div className='mt-3 text-xl'>يؤخذ كمية بسيطة من sickiling powder وتوضع على container نظيف فارغ, ثم إضافة DW إلى نهاية ال container</div>
 
       <div className='flex justify-center mt-2'>
@@ -24,16 +24,22 @@ export default function Special() {
       </div>
 
       <div className='mt-3 text-xl'>يؤخذ drop من المحلول في slide ثم drop من عينة دم edta container وعمل mix ب tip أو wooden stick ثم وضع cover glass وقراءته بعدسة 40x</div>
+      <Image className='w-40 h-40 mt-2 m-auto' src='images/sample-reagent-for-sickle.jpg' alt='img' />
+      <div className='text-center'>sample & reagent</div>
 
-      <div className='text-xl mt-3'>في حالة كانت النتيجة -ve تترك ال slide ساعتين ثم تعاد قراءتها، وإذا كانت -ve توضع ال slide في betri dish فيه شاش مبلول</div>
+      <div className='text-xl mt-3'>في حالة كانت النتيجة -ve تترك ال slide ساعتين ثم تعاد قراءتها، وإذا كانت -ve توضع ال slide في betri dish فيه شاش مبلول ثم تعاد قراءته</div>
+      <Image className='w-80 h-40 mt-2 m-auto' src='images/sickle-test.jpg' alt='img' />
+      <div className='text-center'>sickle test</div>
+      <Image className='w-60 h-40 mt-3 m-auto' src='images/sickiling-test.jpg' alt='img' />
+      <div className='text-center'>sickiling test 40x</div>
 
 
-      <div className='mt-4 text-2xl font-semibold mt-6'><span className='text-[red]'>2- </span>Reticulocyte count</div>
-      <div className='mt-3 text-xl'><span className='text-[red] font-semibold'>في حالة Hb normal </span> يؤخذ 1drop من الدم مع 1drop من ال stain</div>
-      <div className='mt-2 text-xl'><span className='text-[red] font-semibold'>في حالة Hb low </span> يؤخذ 2drop من الدم مع 1drop من ال stain</div>
-      <div className='mt-2 text-xl'><span className='text-[red] font-semibold'>في حالة Hb high </span> يؤخذ 1drop من الدم مع 2drop من ال stain</div>
+      <div className='mt-4 text-2xl font-semibold mt-6'><span className='text-red'>2- </span>Reticulocyte count</div>
+      <div className='mt-3 text-xl'><span className='text-red font-semibold'>في حالة Hb normal </span> يؤخذ 1drop من الدم مع 1drop من ال stain</div>
+      <div className='mt-2 text-xl'><span className='text-red font-semibold'>في حالة Hb low </span> يؤخذ 2drop من الدم مع 1drop من ال stain</div>
+      <div className='mt-2 text-xl'><span className='text-red font-semibold'>في حالة Hb high </span> يؤخذ 1drop من الدم مع 2drop من ال stain</div>
       <div className='mt-2 text-xl'>في plain container وتترك في water path لمدة 15-20 min</div>
-      <div className='text-xl mt-3'><span className='text-[red] font-semibold'>عمل thin blood film </span>ثم حساب ret و rbcs في 10 مربعات (والمربع هو field) كالآتي</div>
+      <div className='text-xl mt-3'><span className='text-red font-semibold'>عمل thin blood film </span>ثم حساب ret و rbcs في 10 مربعات (والمربع هو field) كالآتي</div>
       <div className='mt-2 text-xl'>حساب ال ret في كل مربع في ال 10 مربعات، وحساب ret و rbcs في 3 مربعات، ثم حساب المتوسط لل ret ولل rbcs</div>
       <div className='mt-2 text-xl'>rbcs = مجموع العدد / 3</div>
       <div className='mt-2 text-xl'>ret = مجموع العدد / 10</div>
@@ -41,15 +47,15 @@ export default function Special() {
       <div className='mt-2 text-xl'>ret count = rbcs / ret</div>
 
 
-      <div className='mt-4 text-2xl font-semibold mt-6'><span className='text-[red]'>3- </span>فحص Bone marrow</div>
+      <div className='mt-4 text-2xl font-semibold mt-6'><span className='text-red'>3- </span>فحص Bone marrow</div>
       <div className='text-xl mt-3'>أولا يتم أخذ بيانات المريض (اسم المريض كاملا، العمر، السكن الأصلي، السكن الحالي، الأمراض المزمنة، رقم جوال، رقم جوال ثاني)</div>
       <Image className='w-80 h-34 mt-2 m-auto' src='images/information-bm.jpg' alt='img' />
       <div className='text-center'>بيانات المريض</div>
 
       <div className='mt-3 text-xl'>بعدها يتم أخذ عينة دم من المريض في edta container و plain container وعمل فحوصات cbc و bf و viral screening وتجهيزها قبل يوم العملية وحفظها، وإعطاء المريض أقرب موعد يتواجد فيه الأخصائي لعمل ال bone marrow</div>
 
-      <div className='text-2xl font-semibold text-[red] mt-4'>يوم عملية ال bone marrow</div>
-      <div className='text-xl mt-3'><span className='font-semibold text-[red]' >تجهيز الأدوات </span>3-5 syrringes من المحلول المعقم، تجفيفهم والتأكد من عدم وجود أي بقايا للمحلول داخل ال syrringe قبل غلقه</div>
+      <div className='text-2xl font-semibold text-red mt-4'>يوم عملية ال bone marrow</div>
+      <div className='text-xl mt-3'><span className='font-semibold text-red' >تجهيز الأدوات </span>3-5 syrringes من المحلول المعقم، تجفيفهم والتأكد من عدم وجود أي بقايا للمحلول داخل ال syrringe قبل غلقه</div>
       
       <div className='flex justify-center mt-2'>
         <div>
@@ -66,13 +72,13 @@ export default function Special() {
       <Image className='w-80 h-28 m-auto' src='images/shash-for-bm.jpg' alt='img' />
       <div className='text-center'>تجهيز الشاش</div>
 
-      <div className='text-xl mt-3 font-semibold text-[red]'>تجهيز الدفتر</div>
+      <div className='text-xl mt-3 font-semibold text-red'>تجهيز الدفتر</div>
       <div className='text-xl mt-2'>عمل جدول بالبيانات (رقم المريض، اسم المريض، فحوصات (cbc و bf)، stain, biopsy)</div>
       <div className='text-xl mt-2'>رقم المريض يكتب بترتيب تصاعدي ثم علامة / ثم رقم السنة، ويبدأ من رقم 1 في بداية السنة الجديدة</div>
       <Image className='w-80 h-40 mt-2 m-auto' src='images/registration-for-bm.jpg' alt='img' />
       <div className='text-center'>تجهيز الدفتر</div>
 
-      <div className='text-[red] text-xl mt-4 font-semibold'>أثناء عملية ال BM</div>
+      <div className='text-red text-xl mt-4 font-semibold'>أثناء عملية ال BM</div>
       <div className='text-xl mt-2'>يتم عمل 9 أفلام دم من ال spiration <br />ويوضع ال biopsy في container</div>
       <Image className='w-80 h-40 mt-2 m-auto' src='images/9bloodfilm-for-bm.jpg' alt='img' />
       <div className='text-center'>blood films of aspiration & biopsy</div>
